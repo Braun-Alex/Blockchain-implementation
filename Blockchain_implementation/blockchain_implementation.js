30507c5aab36a663a72911b0dc62f31ed6f7ed08
Braun-Alex <72709229+Braun-Alex@users.noreply.github.com>
2022-07-12 04:50:47 +0300

    Update blockchain_implementation.js

--- Blockchain_implementation/blockchain_implementation.js
+++ Blockchain_implementation/blockchain_implementation.js
@@ -188,3 +188,133 @@ class Transaction
     #setOfOperations;
     #nonce;
 }
+class Block
+{
+    constructor(prevHash, setOfTransactions)
+    {
+        if (setOfTransactions.length === 0) throw Error("Set of transactions must not be empty");
+        if (prevHash === '0000000000000000000000000000000000000000000000000000000000000000')
+            this.#blockHash = SHA3.sha3_256(SHA3.sha3_256((BigInteger.randBetween(BigInteger(1),
+                BigInteger(2).pow(256).minus(1)).toString(16))));
+        else
+        {
+        let merkleContainer = [];
+        for (let transaction in setOfTransactions) merkleContainer.push(transaction.getTransactionHash());
+        while (merkleContainer.length !== 1)
+        {
+            let currentLength = merkleContainer.length;
+            if (currentLength % 2 !== 0) merkleContainer.push(merkleContainer[length - 1]);
+            let merkleHashes = [];
+            for (let i = 0; i < currentLength; i += 2)
+                merkleHashes.push(SHA3.sha3_256(SHA3.sha3_256(
+                    merkleContainer[i] + merkleContainer[i + 1])));
+            merkleContainer = [];
+            for (let merkleHash in merkleHashes) merkleContainer.push(merkleHash);
+        }
+        this.#blockHash = merkleContainer[0];
+        }
+        this.#prevHash = prevHash;
+        this.#setOfTransactions = setOfTransactions;
+    }
+    getBlockHash()
+    {
+        return this.#blockHash;
+    }
+    getPrevHash()
+    {
+        return this.#prevHash;
+    }
+    getSetOfTransactions()
+    {
+        return this.#setOfTransactions;
+    }
+    #blockHash;
+    #prevHash;
+    #setOfTransactions;
+}
+class Blockchain
+{
+    constructor()
+    {
+        let nullHash = '0000000000000000000000000000000000000000000000000000000000000000';
+        let genesisBlock = new Block(nullHash, []);
+        this.#blockHistory.push(genesisBlock);
+    }
+    validateBlock(block)
+    {
+        if (block.getPrevHash() !== this.#blockHistory[this.#blockHistory.length - 1].getBlockHash())
+            return false;
+        let transactions = block.getSetOfTransactions(),
+            temporaryCoinDatabase = new Map();
+        for (let accountAddress in this.#coinDatabase)
+            temporaryCoinDatabase.set(accountAddress, this.#coinDatabase.get(accountAddress));
+        for (let transaction in transactions)
+        {
+            if (this.#txDatabase.has(transaction.getTransactionHash())) return false;
+            let operations = transaction.getTransactionOperations();
+            for (let operation in operations)
+            {
+                if (!Operation.verifyOperation(operation)) return false;
+                let senderAddress = operation.sender.getAddress();
+                if (!temporaryCoinDatabase.has(senderAddress))
+                    temporaryCoinDatabase.set(senderAddress, BigInteger(0));
+                if (temporaryCoinDatabase.get(senderAddress).minus(operation.amount).lesser(BigInteger(0)))
+                    return false;
+                let receiverAddress = operation.receiver.getAddress();
+                if (!temporaryCoinDatabase.has(receiverAddress))
+                    temporaryCoinDatabase.set(receiverAddress, BigInteger(0));
+                temporaryCoinDatabase.set(receiverAddress, this.#coinDatabase.get(receiverAddress).plus(operation.amount));
+            }
+        }
+        for (let transaction in transactions)
+        {
+            let operations = transaction.getTransactionOperations();
+            for (let operation in operations)
+            {
+                let senderAddress = operation.sender.getAddress();
+                if (this.#coinDatabase.has(senderAddress))
+                    temporaryCoinDatabase.set(senderAddress, BigInteger(0));
+                this.#coinDatabase.set(senderAddress,
+                    this.#coinDatabase.get(senderAddress).minus(operation.amount));
+                let receiverAddress = operation.receiver.getAddress();
+                if (!this.#coinDatabase.has(receiverAddress))
+                    this.#coinDatabase.set(receiverAddress, BigInteger(0));
+                this.#coinDatabase.set(receiverAddress, this.#coinDatabase.get(receiverAddress).plus(operation.amount));
+                operation.receiver.updateBalance(operation.amount);
+                this.#txDatabase.set(transaction.getTransactionHash(), transaction);
+            }
+        }
+        this.#blockHistory.push(block);
+        return true;
+    }
+    getCoinsFromFaucet(account, amount)
+    {
+        amount = BigInteger(amount);
+        if (amount.lesserOrEquals(BigInteger(0))) throw Error("Amount to get from faucet must not be lesser or equal zero");
+        if (this.#faucetCoins.divide(amount).lesser(BigInteger(2).pow(128).minus(1)))
+            throw Error("Too big amount to get from faucet");
+        let remainder = this.#faucetCoins.minus(amount);
+        if (remainder.lesser(BigInteger(0))) throw Error("Faucet has no coins");
+        let accountAddress = account.getAddress();
+        if (!this.#coinDatabase.has(accountAddress))
+            this.#coinDatabase.set(accountAddress, BigInteger(0));
+        this.#coinDatabase.set(accountAddress, this.#coinDatabase.get(accountAddress).plus(amount));
+        account.updateBalance(amount);
+    }
+    getCoinDatabase()
+    {
+        return this.#coinDatabase;
+    }
+    getBlockHistory()
+    {
+        return this.#blockHistory;
+    }
+    getTxDatabase()
+    {
+        return this.#txDatabase;
+    }
+    #coinDatabase = new Map();
+    #blockHistory = [];
+    #txDatabase = new Map();
+    #faucetCoins = BigInteger(2).pow(256).minus(1);
+}
